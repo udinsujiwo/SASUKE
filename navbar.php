b75a5b8e1f1d8285f903967087ad454516c6a01c
Udin <akui17825@gmail.com>
2024-06-24 20:17:45 +0700

    protfolio3

--- navbar.php
+++ navbar.php
@@ -12,7 +12,7 @@
               <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
               <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
               <li><a class="nav-link scrollto" href="#services">Layanan</a></li>
-              <li><a class="nav-link scrollto " href="#Portfolio">Portfolio</a></li>
+              <li><a class="nav-link scrollto " href="#Portfolio.php">Portfolio</a></li>
               <li><a class="nav-link scrollto" href="#pricing">Harga</a></li>
               <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
                 <ul>
